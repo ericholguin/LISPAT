cc509454876c36ac85bccbad3be2a2e096d05833
ZugNZwang <eric.holguinsanchez@gmail.com>
2019-05-07 17:23:45 -0600

    Fixed graph issue

--- app.py
+++ app.py
@@ -29,6 +29,17 @@ ALLOWED_EXTENSIONS = {'pdf', 'doc', 'docx'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['ALLOWED_EXTENSIONS'] = ALLOWED_EXTENSIONS
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
 def allowed_file(filename):
     """Summary: For a given file, return whether it's an allowed type."""
